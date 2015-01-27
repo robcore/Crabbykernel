1793442fdb98d1b0b26dca8edb922bfe4de6ca9e
Ashiqur <ashikrobi@gmail.com>
2015-01-27 22:52:28 +0600

    Fastcharge: fix and update USB fastcharge

--- drivers/power/pm8921-sec-charger.c
+++ drivers/power/pm8921-sec-charger.c
@@ -34,6 +34,16 @@
 
 #include <mach/msm_xo.h>
 #include <mach/msm_hsusb.h>
+#include <mach/msm8930-gpio.h>
+
+#ifdef CONFIG_FORCE_FAST_CHARGE
+#include <linux/fastchg.h>
+#endif
+
+#include <linux/battery/sec_charging_common.h>
+#include <linux/i2c/tsu6721.h>
+
+#define PM8917_SEC_CHARGER_CODE
 
 #define CHG_BUCK_CLOCK_CTRL	0x14
 #define CHG_BUCK_CLOCK_CTRL_8038	0xD
@@ -2059,6 +2069,34 @@ static void __pm8921_charger_vbus_draw(unsigned int mA)
 			i--;
 		if (i < 0)
 			i = 0;
+
+#ifdef CONFIG_FORCE_FAST_CHARGE
+		if (force_fast_charge == 1)
+			i = 14;
+		else if (force_fast_charge == 2) {
+			switch (fast_charge_level) {
+				case FAST_CHARGE_500:
+					i = 2;
+					break;
+				case FAST_CHARGE_700:
+					i = 4;
+					break;
+				case FAST_CHARGE_900:
+					i = 8;
+					break;
+				case FAST_CHARGE_1100:
+					i = 10;
+					break;
+				case FAST_CHARGE_1300:
+					i = 12;
+					break;
+				case FAST_CHARGE_1500:
+					i = 14;
+					break;
+				default:
+					break;
+			}
+		}
 		rc = pm_chg_iusbmax_set(the_chip, i);
 		if (rc)
 			pr_err("unable to set iusb to %d rc = %d\n", i, rc);
