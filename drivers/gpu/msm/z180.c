f87afc371bb2dea99d543e1c14368e1cbb86b0ac
Ashiqur <ashikrobi@gmail.com>
2015-02-02 16:36:19 +0600

    msm: kgsl: Fix for panic in z180 postmortem dumps

--- drivers/gpu/msm/z180.c
+++ drivers/gpu/msm/z180.c
@@ -248,22 +248,12 @@ static void z180_cleanup_pt(struct kgsl_device *device,
 			       struct kgsl_pagetable *pagetable)
 {
 	struct z180_device *z180_dev = Z180_DEVICE(device);
-#if !defined(CONFIG_MSM_IOMMU) && defined(CONFIG_SEC_PRODUCT_8960)
-	kgsl_mmu_unmap(pagetable, &device->mmu.setstate_memory);
-	kgsl_mmu_put_gpuaddr(pagetable, &device->mmu.setstate_memory);
 
-	kgsl_mmu_unmap(pagetable, &device->memstore);
-	kgsl_mmu_put_gpuaddr(pagetable, &device->memstore);
-
-	kgsl_mmu_unmap(pagetable, &z180_dev->ringbuffer.cmdbufdesc);
-	kgsl_mmu_put_gpuaddr(pagetable, &z180_dev->ringbuffer.cmdbufdesc);
-#else
 	kgsl_mmu_unmap(pagetable, &device->mmu.setstate_memory);
 
 	kgsl_mmu_unmap(pagetable, &device->memstore);
 
 	kgsl_mmu_unmap(pagetable, &z180_dev->ringbuffer.cmdbufdesc);
-#endif
 }
 
 static int z180_setup_pt(struct kgsl_device *device,
@@ -296,15 +286,9 @@ static int z180_setup_pt(struct kgsl_device *device,
 
 error_unmap_dummy:
 	kgsl_mmu_unmap(pagetable, &device->mmu.setstate_memory);
-#if !defined(CONFIG_MSM_IOMMU) && defined(CONFIG_SEC_PRODUCT_8960)
-	kgsl_mmu_put_gpuaddr(pagetable, &device->mmu.setstate_memory);
-#endif
 
 error_unmap_memstore:
 	kgsl_mmu_unmap(pagetable, &device->memstore);
-#if !defined(CONFIG_MSM_IOMMU) && defined(CONFIG_SEC_PRODUCT_8960)
-	kgsl_mmu_put_gpuaddr(pagetable, &device->memstore);
-#endif
 
 error:
 	return result;
@@ -857,13 +841,29 @@ static int z180_waittimestamp(struct kgsl_device *device,
 				unsigned int msecs)
 {
 	int status = -EINVAL;
+	long timeout = 0;
 
 	/* Don't wait forever, set a max (10 sec) value for now */
 	if (msecs == -1)
 		msecs = 10 * MSEC_PER_SEC;
 
 	mutex_unlock(&device->mutex);
-	status = z180_wait(device, context, timestamp, msecs);
+	timeout = wait_io_event_interruptible_timeout(
+			device->wait_queue,
+			kgsl_check_timestamp(device, context, timestamp),
+			msecs_to_jiffies(msecs));
+
+	if (timeout > 0)
+		status = 0;
+	else if (timeout == 0) {
+		status = -ETIMEDOUT;
+		mutex_lock(&device->mutex);
+		kgsl_pwrctrl_set_state(device, KGSL_STATE_HUNG);
+		kgsl_postmortem_dump(device, 0);
+		mutex_unlock(&device->mutex);
+	} else
+		status = timeout;
+
 	mutex_lock(&device->mutex);
 
 	return status;
